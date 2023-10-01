87b4902616a5c673fc4cf575ccc0f612cea1b66c
flyingtadpole <archer.curry@northwestern.edu>
2023-10-01 17:41:48 -0500

    code in working format, adding in tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest')
+const {Circle, Triangle, Square} = require("./lib/shapes")
 
 const userOptions = [
     {
@@ -31,17 +32,53 @@ const userOptions = [
     },
 ]
 
+const createShape = (response) => {
+    const iconShape = response.iconShape
+    const iconColor = response.shapeColor
+    let newShape = ""
+
+    if (iconShape === "Circle") {
+        newShape = new Circle(iconColor)
+    } else if (iconShape === "Triangle") {
+        newShape = new Triangle(iconColor)
+    } else if (iconShape === "Square") {
+        newShape = new Square(iconColor)
+    }
+
+    return(newShape)
+}
+
+const createText = (response) => {
+    const iconText = response.iconText
+    const textColor = response.textColor
+
+    const newText = `<text x="150" y="125" font-size="50" text-anchor="middle" fill=${textColor}>${iconText}</text>`
+    return(newText)
+}
+
+const writeToSvg = (generatedShape, generatedText) =>
+`<svg 
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
+${generatedShape}
+${generatedText}
+</svg>`
+
 function init() {
     inquirer
     .prompt(userOptions)
     .then ((response) => {
-        const svgFileContent = writeToSvg(response)
+        console.log('response:',response)
+        const generatedShape = createShape(response)
+        console.log("gen shape:", generatedShape)
+        const generatedText = createText(response)
+        console.log('gen text', generatedText)
+        const svgFileContent = writeToSvg(generatedShape, generatedText)
 
         fs.writeFile("logo.svg", svgFileContent,
             (err) =>
             err ? console.log(err) : console.log("Generated logo.svg"))
     })
 }
-
 
 init()
